4f973751bc4161c5f4b94193f324fb275fe63d8f
Nagar-Nivedan <kaustubhsrivastavajee22@gmail.com>
2025-06-24 12:53:06 +0530

    Home page

--- Notes/views.py
+++ Notes/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def notes_entry(request):
+
+    
+    context = {"context": "Welcome to MindVault"}
+    return render(request, "index.html", context)
